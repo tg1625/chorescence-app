cbcf6aa24ac654f7c1c2fb8b111403d35fba7dcf
tg1625 <35944491+tg1625@users.noreply.github.com>
2021-04-07 17:19:19 -0400

    connect group dashboard to backend

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Dashboard extends Component {
     constructor(props){
@@ -7,13 +8,37 @@ class Dashboard extends Component {
             groups: []
         }
     }
+    componentDidMount(){
+        axios.get(`https://chorescence-api.herokuapp.com/user/?id=1`) 
+        .then(response => {
+            let out = [];
+            // console.log(response);
+            for(let i = 0; i < response.data.groups.length; i++){
+                axios.get(`https://chorescence-api.herokuapp.com/group/?id=${response.data.groups[i]}`)
+                .then(res => {
+                    // console.log("Group info:",res);
+                    out.push({id: res.data.id, name: res.data.name})
+                    this.setState({groups: out});
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            }
+            // console.log("Full info", out);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
     render() {
+        console.log("Rendering groups:",this.state.groups);
+        const gs = this.state.groups.map(function(g){
+            return <div className="card"><a href={`group/?group=${g.id}`}>{g.name}</a></div>;
+          });
         return (
         <div className="mainWrapper">
             <h1>Groups</h1>
-            {this.state.groups && this.state.groups.map((g, i) => (
-                <div className="card"><a href={`group/?group=${g.id}`}>{g.name}</a></div>
-            ))}
+            {gs}
             <div className="card"><a href="/joingroup">Join a Group</a></div>
             <div className="card"><a href="/creategroup">Create a Group</a></div>
         </div>
